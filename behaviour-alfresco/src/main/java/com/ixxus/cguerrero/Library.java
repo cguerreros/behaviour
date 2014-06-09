09dc8f233daa23b2054b9efbae1633e183c20d2a
Clara <clara.guerrero@ixxus.com>
2014-06-09 12:09:24 +0100

    Fix some major sonar issues

--- behaviour-alfresco/src/main/java/com/ixxus/cguerrero/Library.java
+++ behaviour-alfresco/src/main/java/com/ixxus/cguerrero/Library.java
@@ -14,106 +14,102 @@ import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 
-public class Library implements NodeServicePolicies.OnCreateNodePolicy, NodeServicePolicies.OnUpdateNodePolicy, NodeServicePolicies.OnAddAspectPolicy {
+public class Library implements NodeServicePolicies.OnCreateNodePolicy,
+		NodeServicePolicies.OnUpdateNodePolicy,
+		NodeServicePolicies.OnAddAspectPolicy {
 
-	//Log
+	// Log
 	private static Logger log = Logger.getLogger(Library.class);
-	
+
 	// Dependencies
 	private NodeService nodeService;
 	private PolicyComponent policyComponent;
 	private ActionService actionService;
 
-	// Behaviours
-	private Behaviour onCreateNode;
-	private Behaviour onUpdateNode;
-	private Behaviour onAddAspect;
+	public void init() {
+		// Behaviours
+		Behaviour onCreateNode;
+		Behaviour onUpdateNode;
+		Behaviour onAddAspect;
 
-	
-	public void init(){
-		//create the behaviours.
+		// create the behaviours.
 		log.debug("Method init start.");
 
-		onCreateNode = new JavaBehaviour(this,"onCreateNode", NotificationFrequency.TRANSACTION_COMMIT);//FIRST_EVENT, EVERY_EVENT.
-		onUpdateNode = new JavaBehaviour(this,"onUpdateNode", NotificationFrequency.EVERY_EVENT);
-		onAddAspect = new JavaBehaviour(this, "onAddAspect", NotificationFrequency.EVERY_EVENT);
-		//onDeleteNode = new JavaBehaviour(this,"onDeleteNode", NotificationFrequency.TRANSACTION_COMMIT);
-		
-		//bind behaviours to node policies.
-		
-		this.policyComponent.bindClassBehaviour(
-				QName.createQName(NamespaceService.ALFRESCO_URI,"onCreateNode"),
-				"cm:content",
-				this.onCreateNode);
-		
-		this.policyComponent.bindClassBehaviour(
-				QName.createQName(NamespaceService.ALFRESCO_URI,"onUpdateNode"),
-				"cm:content",
-				this.onUpdateNode);
-		this.policyComponent.bindClassBehaviour(
-				QName.createQName(NamespaceService.ALFRESCO_URI,"onAddAspect"),
-				"cm:content",
-				this.onAddAspect);
+		onCreateNode = new JavaBehaviour(this, "onCreateNode",
+				NotificationFrequency.TRANSACTION_COMMIT);
+		onUpdateNode = new JavaBehaviour(this, "onUpdateNode",
+				NotificationFrequency.EVERY_EVENT);
+		onAddAspect = new JavaBehaviour(this, "onAddAspect",
+				NotificationFrequency.EVERY_EVENT);
+
+		// bind behaviours to node policies.
+		this.policyComponent.bindClassBehaviour(QName.createQName(
+				NamespaceService.ALFRESCO_URI, "onCreateNode"), "cm:content",
+				onCreateNode);
+
+		this.policyComponent.bindClassBehaviour(QName.createQName(
+				NamespaceService.ALFRESCO_URI, "onUpdateNode"), "cm:content",
+				onUpdateNode);
+		this.policyComponent
+				.bindClassBehaviour(QName.createQName(
+						NamespaceService.ALFRESCO_URI, "onAddAspect"),
+						"cm:content", onAddAspect);
 		log.debug("Method init end.");
 	}
-	
+
 	@Override
 	public void onCreateNode(ChildAssociationRef childAssocRef) {
-		
+
 		log.debug("onCreateNode start");
-		Action addAspectAction = actionService.createAction(AddAspectActionExecuter.NAME);
-		addAspectAction.setParameterValue(AddAspectActionExecuter.PARAM_ASPECT_NAME, LibraryModelI.ASPECT_BH_LIBRARY);
-		log.debug("Before execute action "+AddAspectActionExecuter.NAME);
-		
+		Action addAspectAction = actionService
+				.createAction(AddAspectActionExecuter.NAME);
+		addAspectAction.setParameterValue(
+				AddAspectActionExecuter.PARAM_ASPECT_NAME,
+				LibraryModelI.ASPECT_BH_LIBRARY);
+		log.debug("Before execute action " + AddAspectActionExecuter.NAME);
+
 		NodeRef nodeRef = childAssocRef.getChildRef();
-		actionService.executeAction(addAspectAction,nodeRef,false,true );
-		log.debug("After execute action "+AddAspectActionExecuter.NAME);
+		actionService.executeAction(addAspectAction, nodeRef, false, true);
+		log.debug("After execute action " + AddAspectActionExecuter.NAME);
 		log.debug("onCreateNode end");
 	}
 
 	@Override
 	public void onUpdateNode(NodeRef nodeRef) {
 		log.debug("Setting properties onUpdateNode");
-	    setPropertiesAspect(nodeRef);
+		setPropertiesAspect(nodeRef);
 	}
-
 
 	@Override
 	public void onAddAspect(NodeRef nodeRef, QName aspectTypeQName) {
 		log.debug("Setting properties onAddAspect");
-	    setPropertiesAspect(nodeRef);
-		
+		setPropertiesAspect(nodeRef);
+
 	}
 
-	
-	private void setPropertiesAspect(NodeRef parentRef){
-		
-		  // check the parent to make sure it has the right aspect
-	    if (nodeService.exists(parentRef) && nodeService.hasAspect(parentRef, QName.createQName(LibraryModelI.NAMESPACE,LibraryModelI.ASPECT_BH_LIBRARY))) {
-	        // set the properties that we want.
-	    	  log.debug("Setting aspect properties in the  Node." + parentRef);
-	    	 nodeService.setProperty(
-	    		        parentRef,
-	    		        QName.createQName(
-	    		        		LibraryModelI.NAMESPACE,
-	    		                LibraryModelI.PROP_BOOKED),
-	    		        true);
-	    	 nodeService.setProperty(
-	    		        parentRef,
-	    		        QName.createQName(
-	    		        		LibraryModelI.NAMESPACE,
-	    		                LibraryModelI.PROP_EXPEDIENT),
-	    		        "success");
-	    } else {
-	    	   log.debug("Does not exists the aspect on the node.");
-	        return;
-	    
-	    }
-	    
-	    return;
+	private void setPropertiesAspect(NodeRef parentRef) {
+
+		// check the parent to make sure it has the right aspect
+		if (nodeService.exists(parentRef)
+				&& nodeService.hasAspect(parentRef, QName.createQName(
+						LibraryModelI.NAMESPACE,
+						LibraryModelI.ASPECT_BH_LIBRARY))) {
+			// set the properties that we want.
+			log.debug("Setting aspect properties in the  Node." + parentRef);
+			nodeService.setProperty(parentRef, QName.createQName(
+					LibraryModelI.NAMESPACE, LibraryModelI.PROP_BOOKED), true);
+			nodeService.setProperty(parentRef, QName.createQName(
+					LibraryModelI.NAMESPACE, LibraryModelI.PROP_EXPEDIENT),
+					"success");
+		} else {
+			log.debug("Does not exists the aspect on the node.");
+			return;
+
+		}
+
+		return;
 	}
-	
-	
+
 	public void setNodeService(NodeService nodeService) {
 		this.nodeService = nodeService;
 	}
@@ -125,6 +121,5 @@ public class Library implements NodeServicePolicies.OnCreateNodePolicy, NodeServ
 	public void setActionService(ActionService actionService) {
 		this.actionService = actionService;
 	}
-	
 
 }
--- behaviour-alfresco/src/main/java/com/ixxus/cguerrero/LibraryModelI.java
+++ behaviour-alfresco/src/main/java/com/ixxus/cguerrero/LibraryModelI.java
@@ -1,17 +1,16 @@
 package com.ixxus.cguerrero;
 
- interface LibraryModelI {
+interface LibraryModelI {
 
-	 static final String NAMESPACE ="http://www.ixxus.co.uk/model/behaviour/1.0";
-	 // Aspects
-     static final String ASPECT_BH_LIBRARY = "libraryAspect";
-    
-    // Properties
-     static final String PROP_EXPEDIENT = "expedient";
-     static final String PROP_SECTION = "section";
-     static final String PROP_= "";
-     static final String PROP_BOOKED= "booked";
-     static final String PROP_DATE_BOOKED= "dateBooked";
-    
-	
+	final String NAMESPACE = "http://www.ixxus.co.uk/model/behaviour/1.0";
+	// Aspects
+	final String ASPECT_BH_LIBRARY = "libraryAspect";
+
+	// Properties
+	final String PROP_EXPEDIENT = "expedient";
+	final String PROP_SECTION = "section";
+	final String PROP_ = "";
+	final String PROP_BOOKED = "booked";
+	final String PROP_DATE_BOOKED = "dateBooked";
+
 }
